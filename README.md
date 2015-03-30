1efccc73e373d7c2a2f9aa976836c7818316e96f
PeterAronZentai <peter.zentai@avangrade.com>
2015-03-30 02:23:24 +0100

    jobinfo.search.facets method, test, minidocs employer method, test, docs employer.search method, test, docs

--- README.md
+++ README.md
@@ -17,6 +17,10 @@ Friendly node.js client for the Madgex online service
 ###beware
 the structure of the API tree will probably change
 
+###service description
+The client API is dynamically built by code from the service description config file.
+Extend this to add new functions to the API. (/lib/rest-api-service-description.json)
+
 # API documentation
 
 ##jobinfo(params, done)
@@ -42,3 +46,26 @@ keywords | free text with boolean expressions allowed, optional
 dataFrom | ISO format date
 dateTi | ISO format date
 
+...and much more. refer to the Magic REST documentation for full set of params.
+
+
+##jobinfo.search.full(params, done)
+Same as search but returns full dataset
+
+##jobinfo.search.facets(params, done)
+Return search refiners for a search result. Params are same as in search()
+
+
+
+##employer(params, done)
+Displays information about am employer
+
+####params
+a hash with the following fields
+
+field | type,info
+--- | ---
+id | integer, required
+
+##employer.search(params, done)
+Searches in the employer database
--- test/rest-api-tests.js
+++ test/rest-api-tests.js
@@ -3,38 +3,89 @@
     config = require('./service-config.json')
 
 describe('Madgex module', function () {
-    var client = madgex.createClient(config.serviceName, config.credentials);
+    var client = madgex.createClient(config.serviceName, config.credentials).restApi;
     it("should provide the REST API facede", function () {
-        assert.ok(client.restApi, "restApi not found");
+        assert.ok(client, "restApi not found");
     });
     describe("REST API facade", function () {
+        
         it("should have a jobInfo API part", function () {
-            assert.ok(client.restApi.jobinfo, "jobInfo api branch not found");
+            assert.ok(client.jobinfo, "jobInfo api branch not found");
         });
         describe("jobinfo API function", function () {
             it("shoud be invokable", function () {
-                assert.equal(typeof client.restApi.jobinfo, "function", "jobinfo is not a function");
+                assert.equal(typeof client.jobinfo, "function", "jobinfo is not a function");
             })
             it("should return info on a job", function (done) {
-                client.restApi.jobinfo({ jobid: 1257 }, function (err, data) {
+                client.jobinfo({ jobid: 1257 }, function (err, data) {
                     assert.equal(null, err, "error is not null");
                     assert.equal(data.id, 1257, "jobid id does not match");
                     done();
                 });
             })
             it("should have a search function", function () {
-                assert.ok(client.restApi.jobinfo, "jobInfo api branch not found");
+                assert.ok(client.jobinfo, "jobInfo api branch not found");
             });
-            describe("search API function ", function () {
+            describe("jobinfo.search API function ", function () {
                 it("should return 30 items without params", function (done) {
-                    client.restApi.jobinfo.search({}, function(err, data) {
+                    client.jobinfo.search({}, function(err, data) {
                         assert.ok(data.jobs, "search yielded not jobs");
                         assert.equal(data.jobs.length, 30, "search resulted incorred number of items");
                         done(); 
                     });
                 });
+                it("should have a full subfunction", function () {
+                    assert.ok(client)
+                });
+                it("should have a facets subfunction", function () {
+                    assert.ok(client)
+                });
+                
+                describe("jobinfo.search.full API function", function () {
+                    it("should return 30 items without params", function (done) {
+                        client.jobinfo.search.full({}, function (err, data) {
+                            assert.equal(data.jobs.length, 30, "jobs item count mismatch")
+                            done();
+                        });
+                    });
+                });
+                
+                describe("jobinfo.search.facets API function", function () {
+                    it("should return jobType facet with 20 items without params", function (done) {
+                        client.jobinfo.search.facets({}, function (err, data) {
+                            assert.equal(data.jobType.length, 20, "jobs item count mismatch")
+                            //done();
+                            //console.log(data.jobType.length);
+                            done();
+                        });
+                    });
+                });
             });
         });
+
+        it("should have an employer API part", function () {
+            assert.ok(client.employer, "employer api branch not found");
+        });
+
+        describe("employer API function", function () {
+            it("should be invokable", function () {
+
+            });
+            it("should have a search function", function () {
+                assert.equal(typeof client.employer,"function", "employer must be a function");
+            });
+
+            describe("employer.search", function () {
+                it("should find employers without params", function (done) {
+                    client.employer.search({}, function (err, data) {
+                        assert.equal(data.employers.length, 30, "employers count mismatch");
+                        done();
+                    });
+                });
+
+            })
+        });
+
     })
 })
 
